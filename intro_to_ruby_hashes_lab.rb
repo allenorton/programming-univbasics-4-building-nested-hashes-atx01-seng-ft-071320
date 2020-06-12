03100d5e481f5848d68c4e470d517f736fbf383b
Allene Norton <norton.allene@gmail.com>
2020-06-12 22:49:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,9 @@ def monopoly_with_third_tier
     hash1 = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {},
+      :rent_in_dollars => {
+        :one_piece_owned => 25
+      },
       :names => {}
     } 
   }
