6af338949eda61b48a8ab02dc32969d5ec970670
Allene Norton <norton.allene@gmail.com>
2020-06-12 22:57:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,9 @@ def monopoly_with_third_tier
       },
       :names => {
         :reading_railroad => {},
-        :pennsylvania_railroad => {}
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :short_line_railroad => {}
       }
     } 
   }
